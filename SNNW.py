5ee4433859f3624a9f4e10b2bf8ed8e8efa0d286
Henry Peng <pworker.tw@gmail.com>
2016-08-15 17:25:05 +0800

    writeahead rough API

--- SNNW.py
+++ SNNW.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import requests
 import urllib
+import bs4
 app = Flask(__name__)
 
 
@@ -14,6 +15,15 @@ def linggleit(query):
     url = 'http://linggle.com/query/{}'.format(urllib.quote(query, safe=''))
     r = requests.get(url)
     return jsonify(r.text)
+
+
+@app.route("/writeahead/<query>")
+def writeaheadit(query):
+    url ='http://www.writeahead.org/add?text={}'.format(urllib.quote(query, safe=''))
+    r = requests.get(url)
+    soup = bs4.BeautifulSoup(r.text, "html.parser")
+    grammarPatternBlock = soup.find('div', {'id': 'gp_block'}).decode()
+    return grammarPatternBlock
 
 if __name__ == "__main__":
     app.run()
