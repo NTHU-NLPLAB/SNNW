a2f6b55e685dfe15618fd7a03814d2c9f821c7e3
dspp779 <dspp779@gmail.com>
2016-08-19 18:04:01 +0800

    push to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SNNW:app --log-file -
--- SNNW.py
+++ SNNW.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import requests
 import urllib
 import bs4
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 @app.route("/")
@@ -28,6 +29,13 @@ def writeaheadit(query):
     return grammarPatternBlock
 
 
+@app.route("/writeaheadd/<query>")
+def writeahead(query):
+    text = request.args['text'].lower().encode('utf8').translate(string.maketrans("",""), string.punctuation).strip().split()
+    r = requests.get(url)
+    return r.text
+
+
 @app.route("/rephraser/<query>")
 def rephraseit(query):
     url = 'http://ironman.nlpweb.org:13142/get_paraphrases/{}'.format(urllib.quote(query, safe=''))
@@ -35,4 +43,4 @@ def rephraseit(query):
     return jsonify(r.json())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
