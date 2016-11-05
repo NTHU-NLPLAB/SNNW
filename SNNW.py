505e219aa316e0097138474d65bd7d3e8f0fe9f3
JJC <jjc@nlplab.cc>
2016-11-05 09:42:14 +0800

    py3 compatible

--- SNNW.py
+++ SNNW.py
@@ -1,6 +1,6 @@
-import urllib
 import bs4
 import requests
+import string
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
@@ -15,14 +15,14 @@ def index():
 
 @app.route("/linggle/<query>")
 def linggleit(query):
-    url = 'http://linggle.com/query/{}'.format(urllib.quote(query, safe=''))
+    url = 'http://linggle.com/query/{}'.format(query)
     r = requests.get(url)
     return r.text
 
 
 @app.route("/writeahead/<query>")
 def writeaheadit(query):
-    url = 'http://www.writeahead.org/add?text={}'.format(urllib.quote(query, safe=''))
+    url = 'http://www.writeahead.org/add?text={}'.format(query)
     r = requests.get(url)
     soup = bs4.BeautifulSoup(r.text, "html.parser")
     grammarPatternBlock = soup.find('div', {'id': 'gp_block'}).decode()
@@ -39,7 +39,7 @@ def writeaheadit(query):
 @app.route("/rephraser/<query>")
 def rephraseit(query):
     query = ' '.join(query.split()[-4:])
-    url = 'http://ironman.nlpweb.org:13142/get_paraphrases/{}'.format(urllib.quote(query, safe=''))
+    url = 'http://ironman.nlpweb.org:13142/get_paraphrases/{}'.format(query)
     r = requests.get(url)
     return jsonify(r.json())
 
@@ -48,7 +48,8 @@ def rephraseit(query):
 def get_example(resource, query):
     url = 'http://ironman.nlpweb.org:3440/api/{}/{}'.format(resource, query)
     examples = requests.get(url).json()
-    return jsonify({'query': query, 'resource': resource, 'examples': examples})
+    examples = set(sent.strip().strip(string.punctuation).capitalize() for sent in examples)
+    return jsonify({'query': query, 'resource': resource, 'examples': list(examples)})
 
 if __name__ == "__main__":
     app.run(debug=True)
